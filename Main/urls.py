b9a06c62fa699ba23174f3b87151e083048ecf6e
Som-body <OHong@Hawaii.edu>
2015-11-28 15:37:26 -1000

    User Authentication Redirect
    User authentications will redirect to previous pages.

--- Main/urls.py
+++ Main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name = 'index') ,
     url(r'^user/(?P<username>\w+)/$', views.AccountView, name='accountview'),
     url(r'^login/$', 'Main.views.login'),
-    url(r'^logout$', 'Main.views.logout'),
-    url(r'^finish_sign_up$', 'Main.views.finish_sign_up'),
-    url(r'^signup$', views.SignUpView.as_view(), name = 'signup'),
+    url(r'^logout/$', 'Main.views.logout'),
+    url(r'^finish_sign_up/$', 'Main.views.finish_sign_up'),
+    url(r'^signup/$', views.SignUpView.as_view(), name = 'signup'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -21,6 +21,7 @@ class IndexView(generic.ListView):
         return CarouselSlide.objects.all()
  
 def login(request):
+    redirect_to = request.REQUEST.get('next', '/')
     if request.method == "POST":
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
@@ -28,31 +29,40 @@ def login(request):
         
         if user is not None:
             auth.login(request, user)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(redirect_to)
         
         else:
-            args = {}
+            args = locals()
             args.update(csrf(request))
             args['error_message'] = "Invalid username and password combination"
             return render_to_response('Main/login.html', args)
         
     else:
-        args = {}
+        args = locals()
         args.update(csrf(request))
         return render_to_response('Main/login.html', args)
     
 def logout(request):
+    redirect_to = request.REQUEST.get('next', '/')
     auth.logout(request)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return HttpResponseRedirect(redirect_to)
    
 class SignUpView(FormView):
     template_name = 'Main/signup.html'
-    form_class = CNRegistrationForm
-    success_url = '/finish_sign_up'
+    form_class = CNRegistrationForm 
+    
+    def get_context_data(self, **kwargs):
+        context = super(FormView, self).get_context_data(**kwargs)
+        context['next_url'] = self.request.GET.get('next') 
+        return context
+    
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return '/finish_sign_up/?next=' + next_url 
+        return '/finish_sign_up/' 
     
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
         form.save()
         user = auth.authenticate(username = self.request.POST.get('username'), 
                                  password = self.request.POST.get('password1'))    
@@ -60,19 +70,20 @@ class SignUpView(FormView):
         return super(SignUpView, self).form_valid(form)
 
 def finish_sign_up(request):
+    redirect_to = request.REQUEST.get('next', '/')
     if request.user.is_authenticated():
         if not UserModel.objects.filter(user = request.user):
             UserModel.objects.create(user = request.user)
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect(redirect_to)
 
 def AccountView(request, username):
     u = User.objects.get(username=username)
     user = request.user
-    args = {}
+    args = locals()
     args.update(csrf(request))
     args['slides'] = CarouselSlide.objects.all()
     args['profile_user'] = u
-    if user:
+    if user.is_authenticated():
         args['user'] = user
     userdata = UserModel.objects.get(user = u)
     args['userdata'] = userdata
